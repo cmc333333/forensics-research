708133b935e79870b2a9e1cfe3e50f7806a8666c
CM Lubinski <cmc@agile>
2013-04-14 17:46:28 -0500

    Don't be so ambitious. Have the proof that this is the only jpeg in the disk

--- jpeg.v
+++ jpeg.v
@@ -2,10 +2,10 @@ Require Import Coq.Arith.EqNat.
 Require Import Coq.Lists.List.
 Require Import Coq.Bool.BoolEq.
 
-Definition is_jpeg_start_tag (img: list nat) (offset_start: nat): bool :=
-  andb (beq_nat (nth offset_start img 0) 255) (beq_nat (nth (offset_start + 1) img 0) 216).
-Definition is_jpeg_end_tag (img: list nat) (offset_start: nat): bool :=
-  andb (beq_nat (nth offset_start img 0) 255) (beq_nat (nth (offset_start + 1) img 0) 217).
+Definition is_jpeg_start_tag (disk: list nat) (offset_start: nat): bool :=
+  andb (beq_nat (nth offset_start disk 0) 255) (beq_nat (nth (offset_start + 1) disk 0) 216).
+Definition is_jpeg_end_tag (disk: list nat) (offset_start: nat): bool :=
+  andb (beq_nat (nth offset_start disk 0) 255) (beq_nat (nth (offset_start + 1) disk 0) 217).
 
 Fixpoint ble_nat n m: bool :=
   match n, m with
@@ -15,34 +15,42 @@ Fixpoint ble_nat n m: bool :=
   | S n', S m' => ble_nat n' m'
 end.
 
-Definition jpeg_start_end_tags (img: list nat) (offset_start offset_end: nat): Set :=
-  {(andb 
-    (andb (is_jpeg_start_tag img offset_start) (is_jpeg_end_tag img offset_end)) 
-    (ble_nat offset_start offset_end)) = true} +
-  {False}.
-
-Definition jpeg_start_end_pair (disk: list nat) (start_end: nat*nat): bool :=
+Definition jpeg_start_end_tags (disk: list nat) (offset_start offset_end: nat): bool :=
   andb 
-    (andb (is_jpeg_start_tag disk (fst start_end)) (is_jpeg_end_tag disk (snd start_end))) 
-    (ble_nat (fst start_end) (snd start_end)). 
+    (andb (is_jpeg_start_tag disk offset_start) (is_jpeg_end_tag disk offset_end)) 
+    (ble_nat offset_start offset_end).
 
-Definition jpeg_start_end_pairs (disk: list nat) (offsets: list (nat*nat)): bool :=
-  forallb (jpeg_start_end_pair disk) offsets.
-
-Definition all_jpegs (disk: list nat) (offsets: list (nat*nat)): Set :=
-  {
-    forall (offset: nat) (limit: offset < length disk),
-      (andb 
-        (orb 
-          (negb (is_jpeg_start_tag disk offset))
-          (existsb (map fst offsets) beq_nat offset)
-          (* not followed by an end *)
+Definition only_jpeg (disk: list nat) (offset_start offset_end: nat): Set :=
+  {and
+    ((jpeg_start_end_tags disk offset_start offset_end) = true)
+    (forall (offset: nat) (limit: offset < length disk),
+      (andb
+        (orb (* Start Tag *)
+          (orb (negb (is_jpeg_start_tag disk offset)) (beq_nat offset offset_start))
+          (orb
+            (andb (* Offset < Start *)
+              (ble_nat offset offset_start)
+              (negb (existsb (is_jpeg_end_tag disk) 
+                (firstn (offset_start - offset) (skipn offset disk))))
+            )
+            (andb (* Offset > End *)
+              (ble_nat offset_end offset)
+              (negb (existsb (is_jpeg_end_tag disk) (skipn offset disk)))
+            )
+          )
         )
-        (orb 
-          (negb (is_jpeg_end_tag disk offset))
-          (existsb (map snd offsets) beq_nat offset)
-          (* not preceeded by a start *)
+        (orb (* End Tag *)
+          (orb (negb (is_jpeg_end_tag disk offset)) (beq_nat offset offset_end))
+          (orb
+            (andb (* Offset < Start *)
+              (ble_nat offset offset_start)
+              (negb (existsb (is_jpeg_start_tag disk) (firstn offset disk)))
+            )
+            (andb (* Offset > End *)
+              (ble_nat offset_end offset)
+              (negb (existsb (is_jpeg_start_tag disk) (skipn offset_end disk)))
+            )
+          )
         )
-      ) 
-
+      ) = true)
   } + { False }.
